4c2c444ffa8697585e98b0fe729ff2e9d63d1d97
Otabek <otabektolaganov69@gmail.com>
2023-12-21 22:32:07 +0500

    21/12/2023 22:32

--- src/slice/auth.js
+++ src/slice/auth.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isLoading: false,
   isLoggedIn: false,
+  error: null,
   user: null,
 };
 
@@ -19,11 +20,22 @@ export const authSlice = createSlice({
     registerUserStart: (state) => {
       state.isLoading = true;
     },
-    registerUserSuccess: (state) => {},
-    registerUserFailure: (state) => {},
+    registerUserSuccess: (state) => {
+      state.isLoading = false;
+      state.isLoggedIn = true;
+    },
+    registerUserFailure: (state) => {
+      state.isLoading = false;
+      state.error = "error";
+    },
   },
 });
 
-export const { loginUserStart, registerUserStart } = authSlice.actions;
+export const {
+  loginUserStart,
+  registerUserStart,
+  registerUserSuccess,
+  registerUserFailure,
+} = authSlice.actions;
 
 export default authSlice.reducer;
